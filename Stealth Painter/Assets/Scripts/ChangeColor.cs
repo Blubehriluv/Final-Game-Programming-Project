fa8613f8d0adcaf82fefb47da376a7eb9be44789
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2019-12-13 04:42:24 -0700

    Tried Didn't Work

---
+++ Stealth Painter/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Schema;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using Vector2 = System.Numerics.Vector2;
+
+public class ChangeColor : MonoBehaviour
+{
+    public float colourChangeDelay = 0.5f;
+    float currentDelay = 0f;
+    bool colourChangeCollision = false;
+    Vector2 newVect;
+    private Transform tf;
+    private Tilemap tilemap;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkColourChange();
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        newVect = new Vector2(tf.position.x, tf.position.y);
+        Debug.Log("Contact was made!");
+        colourChangeCollision = true;
+        currentDelay = Time.time + colourChangeDelay;
+        //tilemap.SetColor(new Vector3Int(y, x, 0), Color.red);
+    }
+
+    void checkColourChange()
+    {
+        if (colourChangeCollision)
+        {
+            transform.GetComponent<Renderer>().material.color = Color.yellow;
+            if (Time.time > currentDelay)
+            {
+                transform.GetComponent<Renderer>().material.color = Color.white;
+                colourChangeCollision = false;
+            }
+        }
+    }
+    
+    /*
+    void TryThis()
+    {
+        tilemap = GameObject.Find("TermalTileMap").GetComponent("Tilemap") as Tilemap;
+
+        for (int y = 0; y < blockTempDataMap.GetLength(0); y++)
+        {
+            for (int x = 0; x < blockTempDataMap.GetLength(1); x++)
+            {
+                tilemap.SetTile(new Vector3Int(y, x, 0), tilemap[0]);
+                tilemap.SetColor(new Vector3Int(y, x, 0), Color.red);
+            }
+        }
+    }*/
+}
---
+++ Stealth Painter/Assets/Scripts/TryChange.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TryChange : MonoBehaviour
+{
+
+    [SerializeField] private TileBase[] tileBase;
+    private Tilemap tileMap;
+
+    private float[,] blockTempDataMap = new float[250, 150];
+
+    void Start()
+    {
+        
+    }
+
+    void ChangeCo()
+    {
+        tileMap = GameObject.Find("Changed Floor").GetComponent("Tilemap") as Tilemap;
+
+        for (int y = 0; y < blockTempDataMap.GetLength(0); y++)
+        {
+            for (int x = 0; x < blockTempDataMap.GetLength(1); x++)
+            {
+                tileMap.SetTile(new Vector3Int(y, x, 0), tileBase[0]);
+                tileMap.SetColor(new Vector3Int(y, x, 0), Color.red);
+            }
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //newVect = new Vector2(tf.position.x, tf.position.y);
+        Debug.Log("Contact was made!");
+        //colourChangeCollision = true;
+        //currentDelay = Time.time + colourChangeDelay;
+        //tilemap.SetColor(new Vector3Int(y, x, 0), Color.red);
+        ChangeCo();
+    }
+
+}
