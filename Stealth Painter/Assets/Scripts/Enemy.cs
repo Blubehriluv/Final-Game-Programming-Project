36f5b97a5fb58da11cc7789e4cc3343a279b6add
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2019-12-16 17:51:27 -0700

    AI is being Developed
    Creating the state machine now

---
+++ Stealth Painter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = System.Numerics.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    private Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    private Vector3 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
+    }
+
+    private void FixedUpdate()
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector3 direction)
+    {
+        rb.MovePosition((Vector3)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
---
+++ Stealth Painter/Assets/Scripts/State Machine/AI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+    public bool switchState = false;
+
+    public StateMachine<AI> stateMachine { get; set; }
+
+    private void Start()
+    {
+        stateMachine = new StateMachine<AI>(this);
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
