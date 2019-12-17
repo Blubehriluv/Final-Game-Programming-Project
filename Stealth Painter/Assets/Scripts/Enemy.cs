909e285f3aa6b5b1dfd3ee4982a2a946699ede07
Taylor McKenzie <xx20twinkiie14xx@gmail.com>
2019-12-16 20:52:54 -0700

    Game is looking better
    Nice.  Real Nice

--- Stealth Painter/Assets/Scripts/Enemy.cs
+++ Stealth Painter/Assets/Scripts/Enemy.cs
@@ -5,36 +5,44 @@ using UnityEngine;
 using Vector2 = System.Numerics.Vector2;
 using Vector3 = UnityEngine.Vector3;
 
-public class Enemy : MonoBehaviour
-{
-    public Transform player;
-    private Rigidbody2D rb;
-    public float moveSpeed = 5f;
-    private Vector3 movement;
-
-    // Start is called before the first frame update
-    void Start()
+ public class Enemy : MonoBehaviour
     {
-        rb = this.GetComponent<Rigidbody2D>();
-    }
+        public Transform player;
+        private Rigidbody2D rb;
+        public float moveSpeed = 5f;
+        private Vector3 movement;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 direction = player.position - transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        rb.rotation = angle;
-        direction.Normalize();
-        movement = direction;
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
+            rb = this.GetComponent<Rigidbody2D>();
+        }
 
-    private void FixedUpdate()
-    {
-        moveCharacter(movement);
-    }
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 direction = player.position - transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+            direction.Normalize();
+            movement = direction;
+        }
 
-    void moveCharacter(Vector3 direction)
-    {
-        rb.MovePosition((Vector3)transform.position + (direction * moveSpeed * Time.deltaTime));
+
+
+        private void FixedUpdate()
+        {
+            moveCharacter(movement);
+        }
+        
+        /*
+        public void GiveChase()
+        {
+            moveCharacter(movement);
+        }*/
+
+        void moveCharacter(Vector3 direction)
+        {
+            rb.MovePosition((Vector3) transform.position + (direction * moveSpeed * Time.deltaTime));
+        }
     }
-}
--- Stealth Painter/Assets/Scripts/State Machine/AI.cs
+++ Stealth Painter/Assets/Scripts/State Machine/AI.cs
@@ -2,21 +2,51 @@
 using System.Collections.Generic;
 using UnityEditorInternal;
 using UnityEngine;
+using StateStuff;
 
 public class AI : MonoBehaviour
 {
     public bool switchState = false;
+    public float gameTimer;
+    public int seconds = 0;
 
     public StateMachine<AI> stateMachine { get; set; }
 
     private void Start()
     {
         stateMachine = new StateMachine<AI>(this);
-
+        stateMachine.ChangeState(FirstState.Instance);
+        gameTimer = Time.time;
     }
 
     private void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other != null)
+        {
+            if (Time.time > gameTimer + 1)
+            {
+                gameTimer = Time.time;
+                seconds++;
+                Debug.Log(seconds);
+            }
+
+            if (seconds == 3)
+            {
+                Debug.Log("Nothing found!");
+                seconds = 0;
+            }
+        }
+
+        if (other == GameObject.FindWithTag("Player"))
+        {
+            Debug.Log("Player found!");
+            switchState = !switchState;
+            stateMachine.Update();
+        }
     }
 }
